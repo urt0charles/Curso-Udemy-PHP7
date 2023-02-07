c19b93ede141c708a1c34d701ea6d3c0a2780d95
Charles Fernandes <charlesfernandeswww97@gmail.com>
2023-02-07 14:32:22 -0300

    operadores_incremento_decremento.php

---
+++ operadores_incremento_decremento.php
@@ -0,0 +1,32 @@
+<h3>Pós-incremento</h3>
+    <?php
+    $a = 7;
+    echo "O valor contido em a é $a <br />";
+    echo "O valor contido em a após o incremento é " . $a++ . "<br/>";
+    echo "O valor atualizado é $a";
+
+    ?>
+    <h3>Pré-incremento</h3>
+    <?php
+    $a = 7;
+    echo "O valor contido em a é $a <br />";
+    echo "O valor contido em a pré o incremento é " . ++$a . "<br/>";
+    echo "O valor atualizado é $a";
+
+    ?>
+    <h3>Pós-decremento</h3>
+    <?php
+    $a = 7;
+    echo "O valor contido em a é $a <br />";
+    echo "O valor contido em a após o decremento é " . $a-- . "<br/>";
+    echo "O valor atualizado é $a";
+
+    ?>
+    <h3>Pré-decremento</h3>
+    <?php
+    $a = 7;
+    echo "O valor contido em a é $a <br />";
+    echo "O valor contido em a pré o decremento é " . --$a . "<br/>";
+    echo "O valor atualizado é $a";
+
+    ?>
